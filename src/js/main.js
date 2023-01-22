6a4eedfed3a0f2004ff384ffa2c3b6fdfc81ad94
Aragon Emanuel <aragon3m4@gmail.com>
2023-01-22 07:49:12 -0300

    Add functions to change page data

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,10 @@ const URL = 'https://randomuser.me/api/';
 const PARAMETERS = '?nat=br,es,mx,us&noinfo';
 
 function changePageData(response) {
+    changeName(response.name);
+    changeImage(response.picture.large);
+    changeAboutMe(response);
+    changeContactInfo(response);
 }
 
 try {
@@ -14,3 +18,31 @@ try {
 } catch (error) { console.error(error); }
 
 
+
+function changeContactInfo(response) {
+    document.querySelector('#cell-number').textContent += `${response.cell}`
+
+    document.querySelector('#phone-number').textContent += `${response.phone}`
+
+    document.querySelector('#email').textContent += `${response.email}`
+}
+
+function changeAboutMe(response) {
+    document.querySelector('#location').textContent +=
+        `${response.location.country}. ` + `${response.location.state}, ` + `${response.location.city}`
+
+    
+    document.querySelector('#gender').textContent += `${response.gender}`.charAt(0).toUpperCase() + `${response.gender}`.slice(1);
+
+    document.querySelector('#age').textContent += `${response.dob.age}`
+
+    document.querySelector('#birthdate').textContent += `${response.dob.date.split('T')[0]}`
+}
+
+function changeName(name) {
+    document.querySelector('#name').textContent = `${name.first} ` + `${name.last}`;
+}
+
+function changeImage(imageSource) {
+    document.querySelector('img').src = imageSource;
+}
